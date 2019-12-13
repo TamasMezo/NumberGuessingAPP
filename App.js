8fdc7f2816af70aacf7e460b62b214fa4b9a3d66
Tamás <codesigner.hu@gmail.com>
2019-12-13 08:30:24 +0100

    Adding Game screen

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  const startGameHandler = () => {};
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number" />
       <StartGameScreen />
+      <GameScreen />
     </View>
   );
 }
---
+++ screens/GameScreen.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+import NumberContainer from "../components/NumberContainer";
+import Card from "../components/Card";
+
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+};
+
+const GameScreen = props => {
+  const [currendGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 100, props.userChoise)
+  );
+
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <NumberContainer>{currendGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button title="Lower" onPress={() => {}} />
+        <Button title="Greater" onPress={() => {}} />
+      </Card>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center"
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: 300,
+    maxWidth: "80%"
+  }
+});
+
+export default GameScreen;
