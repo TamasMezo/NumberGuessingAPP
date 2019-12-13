004d451d2a76a48f88467a4aaf2f56e2dbc4a616
Tamás <codesigner.hu@gmail.com>
2019-12-13 19:47:15 +0100

    rendering screen conditionally

--- App.js
+++ App.js
@@ -8,12 +8,20 @@ import GameScreen from "./screens/GameScreen";
 export default function App() {
   const [userNumber, setUserNumber] = useState();
 
-  const startGameHandler = () => {};
+  const startGameHandler = selectedNumber => {
+    setUserNumber(selectedNumber);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  if (userNumber) {
+    content = <GameScreen userChoise={userNumber} />;
+  }
+
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number" />
-      <StartGameScreen />
-      <GameScreen />
+      {content}
     </View>
   );
 }
