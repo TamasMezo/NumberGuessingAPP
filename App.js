76212c23a65cd9c00202bbb63f12f00f198957dd
Tamás <codesigner.hu@gmail.com>
2019-12-14 12:24:02 +0100

    Adding sign in page to app

--- App.js
+++ App.js
@@ -5,10 +5,12 @@ import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
 import GameOverScreen from "./screens/GameOverScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
 
-export default function App() {
+const App = () => {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGameRounds] = useState(0);
+  const [userEmail, setUserEmail] = useState("");
 
   const startGameHandler = selectedNumber => {
     setUserNumber(selectedNumber);
@@ -24,12 +26,24 @@ export default function App() {
     setUserNumber(0);
   };
 
-  let content = <StartGameScreen onStartGame={startGameHandler} />;
+  const signinHandler = emailAddress => {
+    setUserEmail(emailAddress);
+    console.log("signedIN");
+  };
+
+  let content = <WelcomeScreen onSignIn={signinHandler} />;
+  let header = null;
+
+  if (userEmail && userEmail.length > 5) {
+    content = <StartGameScreen onStartGame={startGameHandler} />;
+    header = <Header title="Guess a Number" />;
+  }
 
   if (userNumber && guessRounds <= 0) {
     content = (
       <GameScreen userChoise={userNumber} onGameOver={gameOverHandler} />
     );
+    header = <Header title="Guess a Number" />;
   } else if (guessRounds > 0) {
     content = (
       <GameOverScreen
@@ -38,18 +52,21 @@ export default function App() {
         onRestart={startNewGame}
       />
     );
+    header = <Header title="Guess a Number" />;
   }
 
   return (
     <View style={styles.screen}>
-      <Header title="Guess a Number" />
+      {header}
       {content}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1
   }
 });
+
+export default App;
