121b442606d630b37eddf9d61b9ee7e2a9c08847
Tamás <codesigner.hu@gmail.com>
2019-12-14 23:45:39 +0100

    adding icon to header

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import bar from "../assets/bar.png";
 
 import Colors from "../constans/colors";
 
 const Header = props => {
   return (
     <View style={styles.header}>
-      <Text style={styles.headerTitle}>{props.title}</Text>
+      <View style={styles.titleContainer}>
+        <Text style={styles.headerTitle}>{props.title}</Text>
+      </View>
+      <View style={styles.barContainer}>
+        <TouchableOpacity>
+          <Image style={styles.bar} source={bar} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -18,11 +26,27 @@ const styles = StyleSheet.create({
     paddingTop: 36,
     backgroundColor: Colors.primary,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    flexDirection: "row"
+  },
+  titleContainer: {
+    width: "70%",
+    alignItems: "flex-end",
+    justifyContent: "flex-end"
   },
   headerTitle: {
     color: "#fff",
     fontSize: 20
+  },
+  barContainer: {
+    flex: 1,
+    marginRight: 20,
+    alignItems: "flex-end",
+    justifyContent: "flex-end"
+  },
+  bar: {
+    width: 20,
+    height: 20
   }
 });
 
