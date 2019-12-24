ca5b36e0038612d546c81605b64ba82bb5f69832
Tamás <codesigner.hu@gmail.com>
2019-12-24 19:44:48 +0100

    using Platform API

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+  Platform
+} from "react-native";
 
 import bar from "../assets/bar.png";
 import Colors from "../constans/colors";
@@ -9,7 +17,7 @@ const Header = props => {
   return (
     <View style={styles.header}>
       <View style={styles.titleContainer}>
-        <TitleText>{props.title}</TitleText>
+        <TitleText style={styles.title}>{props.title}</TitleText>
       </View>
       <View style={styles.barContainer}>
         <TouchableOpacity>
@@ -22,20 +30,24 @@ const Header = props => {
 
 const styles = StyleSheet.create({
   header: {
-    width: "100%",
-    height: 90,
-    paddingTop: 36,
-    backgroundColor: Colors.primary,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height * 0.1,
+    paddingTop: 30,
+    backgroundColor: Platform.OS === "android" ? Colors.primary : "white",
     alignItems: "center",
     justifyContent: "center",
-    flexDirection: "row"
+    flexDirection: "row",
+    borderBottomColor: Platform.OS === "ios" ? "#ccc" : "transparent",
+    borderBottomWidth: Platform.OS === "ios" ? 1 : 0
   },
   titleContainer: {
     width: "70%",
     alignItems: "flex-end",
     justifyContent: "flex-end"
   },
-
+  title: {
+    color: Platform.OS === "ios" ? Colors.secondary : Colors.primary
+  },
   barContainer: {
     flex: 1,
     marginRight: 20,
